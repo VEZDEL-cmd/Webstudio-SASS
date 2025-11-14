9cb55177e5e0a2417cdad4b8684fd6cc526bb7f7
Bohdan Vezdel <vezdel.studio@gmail.com>
2025-11-14 16:33:43 +0200

    first commit

--- modal.js
+++ modal.js
@@ -14,32 +14,4 @@
   }
 })();
  
-// // Всі модалки на сторінці
-// const modals = document.querySelectorAll('[data-modal]');
-
-// modals.forEach(modal => {
-//   const closeBtn = modal.querySelector('[data-modal-close]');
-
-//   // Відкриття модалки (тут приклад для однієї кнопки, можна зробити кілька)
-//   const openBtns = document.querySelectorAll(`[data-modal-open="${modal.id}"]`);
-//   openBtns.forEach(btn => {
-//     btn.addEventListener('click', () => {
-//       modal.classList.remove('is-hidden');
-//       document.body.classList.add('no-scroll');
-//     });
-//   });
-
-//   // Закриття модалки по кнопці
-//   closeBtn.addEventListener('click', () => {
-//     modal.classList.add('is-hidden');
-//     document.body.classList.remove('no-scroll');
-//   });
-
-//   // Закриття модалки при кліку поза контентом
-//   modal.addEventListener('click', (e) => {
-//     if (e.target === modal) {
-//       modal.classList.add('is-hidden');
-//       document.body.classList.remove('no-scroll');
-//     }
-//   });
-// });
+ 
